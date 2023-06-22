24c78e45baa072a61d7883e504b5451f577baf5e
Dan Caron <dancaron@gmail.com>
2023-06-22 09:32:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 # Define function to calculate the evaluation of acid base disorders
 def calculate_disorder(pH, pCO2, HCO3, Albumin, Na, Cl):
@@ -24,18 +22,22 @@ def calculate_disorder(pH, pCO2, HCO3, Albumin, Na, Cl):
     
     return "Normal"
 
-
 # Title of the app
 st.title("Howard Bleich's Evaluation of Acid Base Disorders App")
 
 # User inputs
-pH = st.number_input('Enter pH', min_value=0.0, max_value=14.0)
-pCO2 = st.number_input('Enter pCO2 (mmHg)', min_value=0.0)
-HCO3 = st.number_input('Enter HCO3 (mEq/L)', min_value=0.0)
-Albumin = st.number_input('Enter Albumin (g/dL)', min_value=0.0)
-Na = st.number_input('Enter Na (mEq/L)', min_value=0.0)
-Cl = st.number_input('Enter Cl (mEq/L)', min_value=0.0)
+st.markdown("## Input Parameters")
+st.markdown("Please enter the following laboratory measurements:")
+
+pH = st.number_input('pH (Normal range: 7.35-7.45)', min_value=0.0, max_value=14.0)
+pCO2 = st.number_input('pCO2 in mmHg (Normal range: 35-45 mmHg)', min_value=0.0)
+HCO3 = st.number_input('HCO3 in mEq/L (Normal range: 22-28 mEq/L)', min_value=0.0)
+Albumin = st.number_input('Albumin in g/dL (Normal range: 3.4-5.4 g/dL)', min_value=0.0)
+Na = st.number_input('Na in mEq/L (Normal range: 135-145 mEq/L)', min_value=0.0)
+Cl = st.number_input('Cl in mEq/L (Normal range: 96-106 mEq/L)', min_value=0.0)
 
 if st.button('Calculate'):
     result = calculate_disorder(pH, pCO2, HCO3, Albumin, Na, Cl)
-    st.write(f'The result is: {result}')
+    st.markdown("## Results")
+    st.markdown("The result represents the probable acid-base imbalance based on the given parameters.")
+    st.write(f'The result is: **{result}**')
